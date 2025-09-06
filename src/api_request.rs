b2a3104121e7a889f58151fa802f755b79d4c35f
403f2e <shadowouss1@gmail.com>
2025-09-06 21:50:09 +0100

    feat: update the phf version.

--- src/api_request.rs
+++ src/api_request.rs
@@ -3,7 +3,7 @@ use reqwest;
 use crate::jisho::{AppLog, JishoResponse};
 
 #[tokio::main]
-pub async fn api_handle(keyword: &str) -> Result<JishoResponse, AppLog> {
+pub async fn api_handle(keyword: &'static str) -> Result<JishoResponse, AppLog> {
     let full_url: String = format!("https://jisho.org/api/v1/search/words?keyword={}", keyword);
     let response: JishoResponse = match reqwest::get(&full_url)
         .await
